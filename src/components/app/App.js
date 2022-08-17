f5cbd421969a91b1a13757d204c02050ac647608
sergeykataev1992@gmail.com <Qq650190>
2022-08-17 11:08:13 +0300

    add some staff

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,9 +2,10 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import 'bootstrap/dist/css/bootstrap.css';
 
 import decoration from '../../resources/img/vision.png';
-import {Component} from "react";
+import React, {Component} from "react";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 class App extends Component {
@@ -22,6 +23,16 @@ class App extends Component {
     render() {
         return (
             <div className="app">
+                <DynamicGreetings color={'primary'}>
+                    <h2>This weel was hard</h2>
+                    <h2>Hello world!</h2>
+                </DynamicGreetings>
+                <Counter render={counter=> (
+                    <Message counter={counter}/>
+                )}/>
+                <br/>
+                <br/>
+                <br/>
                 <AppHeader/>
                 <main>
                     <RandomChar/>
@@ -37,6 +48,53 @@ class App extends Component {
         )
     }
 
+}
+
+const DynamicGreetings = (props) => {
+    return (
+        <div className={'mb-3 p-3 border border-' + props.color}>
+            {
+                React.Children.map(props.children, child => {
+                    return React.cloneElement(child, {className: 'shadow p-3 m-3 border rounded'})
+                })
+            }
+        </div>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <h2>The counter is {props.counter}</h2>
+    )
+}
+
+class Counter extends Component {
+    state = {
+        counter: 0
+    }
+
+    changeCounter = () => {
+        this.setState(
+            ({counter}) => ({
+                counter: counter + 1
+            })
+        )
+    }
+
+    render() {
+        return (
+            <>
+                <button
+                    className={'btn btn-primary'}
+                    onClick={this.changeCounter}
+                >
+                    Click me
+                </button>
+                {/*<Message counter={this.state.counter}/>*/}
+                {this.props.render(this.state.counter)}
+            </>
+        )
+    }
 }
 
 export default App;
